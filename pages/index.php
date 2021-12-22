93447ec3a366709a02230f44a3c2df6a8a39acb0
JawwadRaf98 <55888087+JawwadRaf98@users.noreply.github.com>
2021-12-23 00:25:04 +0500

    index edit by jawwad

--- pages/index.php
+++ pages/index.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-Library</title>
 </head>
-<p>edit by jawwad</p>
+
 <body>
     <h1>Edit from Komal</h1>
+    <p>edit by jawwad</p>
 
     
 </body>
