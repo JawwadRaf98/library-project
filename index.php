7c06903351cef9df60d86d5345e7cfe400913ee4
JawwadRaf98 <55888087+JawwadRaf98@users.noreply.github.com>
2021-12-24 02:20:53 +0500

    header work and layout change

--- index.php
+++ index.php
@@ -4,16 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="pages/images/logo.png" type="image/x-icon">
     <title>E-Library</title>
 </head>
 
 <body>
-    <h1>Edit from Komal</h1>
-    <p>edit by jawwad</p>
-    <p>i'm jawwad</p>
+    
+    <?php include('pages/header.php') ?>
+    <h1>i am index</h1>
+    <a href="pages/registration.php">Register</a>
 
-    <a href="registration.php">Register</a>
-
+    <?php include('pages/footer.php') ?>
     
 </body>
 </html>
--- pages/header.php
+++ pages/header.php
@@ -1,17 +1,63 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Registration</title>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="./style.css">
+    <!-- include custom css file -->
+    <link rel="stylesheet" href="pages/style/style.css">
+    <link rel="stylesheet" href="style/style.css">
+  </head>
+  <body>
 
-</head>
-<body>
 
-<main id="main-area">
+  <nav class="navbar navbar-expand-md fixed-top  nav-bg-color ">
+  <div class="container-fluid">
+    <button class="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <!-- logo img -->
+      <a class="navbar-brand" href="#">
+        <img src="pages/images/logo.png" height = 50px width = 50px alt="logo">
+      </a>
+      <ul class="navbar-nav  mb-2 mb-lg-0 nav-list-item">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Partner</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Categories
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+        
+      </ul>
+      <!-- <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form> -->
+    </div>
+  </div>
+</nav>
+ 
