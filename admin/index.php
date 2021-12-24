7f21b98c0c2c7fabee7c2d3fd873255c781c9332
JawwadRaf98 <55888087+JawwadRaf98@users.noreply.github.com>
2021-12-25 03:05:05 +0500

    admin login form added

--- admin/index.php
+++ admin/index.php
@@ -4,9 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>admin</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./stlye/style.css">
+
+    
+    <title>Admin | Login</title>
+
 </head>
 <body>
-    <h1>admin Page<h1>
+    <div class="center-div">
+        <div class="header-div">
+            <p>admin login</p>
+        </div>
+
+        <div class="form">
+            <form action="">
+                <div class="input-field">
+                    <input type="text" id="username" name='username' placeholder="username" />
+                </div>
+                <div class="input-field">
+                    <input type="password" id="password" name='password' placeholder="password"/>
+                </div>
+                <div class="forgot-password">
+                    <a href="">Forgot password ??</a>
+                </div>
+                <div class="submit">
+                    <input type="submit" id="submit" />
+                </div>
+            </form>
+        </div>
+
+    </div>
 </body>
 </html>
