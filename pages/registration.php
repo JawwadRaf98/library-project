0e708dd60e872496fce5ccc6865b3838d855a4db
JawwadRaf98 <55888087+JawwadRaf98@users.noreply.github.com>
2021-12-25 03:16:33 +0500

    user registration page

--- pages/registration.php
+++ pages/registration.php
@@ -1,7 +1,20 @@
-<?php
-    // header.php
-    include ('header.php');
-?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registration</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./style/registration.css">
+
+</head>
+<body>
+
+<main id="main-area">
 
     <?php
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
@@ -21,9 +34,9 @@
                 <div class="upload-profile-image d-flex justify-content-center pb-5">
                     <div class="text-center">
                         <div class="d-flex justify-content-center">
-                            <img class="camera-icon" src="./assets/camera-solid.svg" alt="camera">
+                            <img class="camera-icon" src="./images/camera-solid.svg" alt="camera">
                         </div>
-                        <img src="./assets/profile/avatar.png" style="width: 200px; height: 200px" class="img rounded-circle" alt="profile">
+                        <img src="images/avatar.png" style="width: 200px; height: 200px" class="img rounded-circle" alt="profile">
                         <small class="form-text text-black-50">Choose Image</small>
                         <input type="file" form="reg-form" class="form-control-file" name="profileUpload" id="upload-profile">
                     </div>
@@ -76,7 +89,12 @@
     <!-- #registration area -->
 
 
-<?php
-    // footer.php
-    include ('footer.php');
-?>
+    </main>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+<script src="js/main.js"></script>
+</body>
+</html>
