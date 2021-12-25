3c66ee96c36acf3d6baae572ee131b4f2b6cfc51
JawwadRaf98 <55888087+JawwadRaf98@users.noreply.github.com>
2021-12-25 23:36:57 +0500

    changing in admin login page

--- admin/index.php
+++ admin/index.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
 
     <link rel="stylesheet" href="./stlye/style.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
     
     <title>Admin | Login</title>
@@ -21,11 +22,14 @@
 
         <div class="form">
             <form action="">
-                <div class="input-field">
+                <div class="box">
+                <i class="fa fa-envelope"></i>
                     <input type="text" id="username" name='username' placeholder="username" />
                 </div>
-                <div class="input-field">
+                <div class="box">
+                <i class="fa fa-key"></i>
                     <input type="password" id="password" name='password' placeholder="password"/>
+                    <i class="fa fa-key"></i>
                 </div>
                 <div class="forgot-password">
                     <a href="">Forgot password ??</a>
