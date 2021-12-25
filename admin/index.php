402a6c5ce9b7d987359205b58c3f3be3b74918da
JawwadRaf98 <55888087+JawwadRaf98@users.noreply.github.com>
2021-12-26 01:23:24 +0500

    Admin login page complete

--- admin/index.php
+++ admin/index.php
@@ -7,14 +7,14 @@
 
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
 
-    <link rel="stylesheet" href="./stlye/style.css">
+    <link rel="stylesheet" href="./stlye/login.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
     
     <title>Admin | Login</title>
 
 </head>
-<body>
+<body >
     <div class="center-div">
         <div class="header-div">
             <p>admin login</p>
@@ -24,12 +24,15 @@
             <form action="">
                 <div class="box">
                 <i class="fa fa-envelope"></i>
-                    <input type="text" id="username" name='username' placeholder="username" />
+                    <input type="text" id="username" name='username' placeholder="username" autocomplete="off"/ require>
                 </div>
                 <div class="box">
                 <i class="fa fa-key"></i>
-                    <input type="password" id="password" name='password' placeholder="password"/>
-                    <i class="fa fa-key"></i>
+                    <input type="password" id="password" name='password' placeholder="password" require autocomplete="off"/>
+                   <span class="eye" onclick="passwordToggle()">
+                   <i id = "show" class="far fa-eye" ></i>
+                   <i id = "hide" class="far fa-eye-slash" ></i>
+                   </span>
                 </div>
                 <div class="forgot-password">
                     <a href="">Forgot password ??</a>
@@ -39,7 +42,27 @@
                 </div>
             </form>
         </div>
-
     </div>
+
+    <script>
+       function passwordToggle(){
+        var password =  document.getElementById('password');
+        var show =  document.getElementById('show');
+        var hide =  document.getElementById('hide');
+        console.log(show.style)
+
+       if(password.type === 'password'){
+            password.type = 'text';
+            hide.style.display = 'block';
+            show.style.display = 'none';
+       }
+       else{
+            password.type = 'password';
+            hide.style.display = 'none';
+            show.style.display = 'block';
+       }
+       }
+
+    </script>
 </body>
 </html>
