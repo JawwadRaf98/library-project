bd291157bdedb793eabf3b2c82c7232b907cc99c
JawwadRaf98 <55888087+JawwadRaf98@users.noreply.github.com>
2022-02-15 19:03:40 +0500

    new pass setting

--- admin/profile.php
+++ admin/profile.php
@@ -5,15 +5,55 @@
     include('includes/config.php');
     include('includes/auth.php');
     $data = $_SESSION['userData'];
+
+    $currPassErr = "";
+    $newPassError = "";
+    $updateStatus = false;
+    $updateMessage = "";
+    
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        include 'includes/config.php';
+        
+        $current_password = $_POST['current-pass'];
+        $new_password = $_POST['new-pass'];
+        $confirm_password = $_POST['confirm-pass'];
+        echo $current_password ." ".$data['adminPassword'];  
+
+        if($current_password != $data['adminPassword']){
+            echo $current_password ." ".$data['adminPassword'];  
+            $currPassErr = "Invalid password";
+        }
+        else{
+            echo "comming";
+            $currPassErr = "";
+            if($new_password != $confirm_password){
+                $newPassError = "Password doesn't matched";
+            }
+            else{
+                $newPassError = "";
+                $query = "UPDATE admin SET 'adminPassword' = $new_password  WHERE 'adminEmail' = $new_password";
+                $result = mysqli_query($connection, $query);
+                if($result){
+                    $updateStatus = true;
+                    $updateMessage = "Recode update successfully";
+                }
+            }
+        }
+        
+        
+
+    }
+
+    
     function getName($data){
        return $data['adminName']." ".$data['adminLastName'];
     }
     function getEmail($data){
         return $data['adminEmail'];
-     }
+    }
      function getContact($data){
         return $data['adminContact'];
-     }
+    }
 
 
 ?>
@@ -30,23 +70,59 @@
 
 
         <?php if($_GET['tab'] == 'setting'): ?>
-            <h1>Jawwad</h1>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-8 col-md-12 col-sm-12 col-xs-12">
+                        <?php 
+                            if($updateStatus){
+                                echo '<div class="alert alert-primary" role="alert">';
+                                echo $updateMessage;
+                                echo "</div>";
+                            }
+                        ?>
+
+                    <form action="" method="POST">
+                        <div class="mb-3">
+                            <label for="CurrentPassword" class="form-label">Current Password</label>
+                            <input type="password" name="current-pass" class="form-control" id="CurrentPassword" required>
+                            <small<?php echo $currPassErr ?></small>
+                        </div>
+                        <div class="mb-3">
+                            <label for="newPassword" class="form-label">New Password</label>
+                            <input type="password" name="new-pass"class="form-control" id="newPassword" required>
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="confrimPassword" class="form-label">Comfirm Password</label>
+                            <input type="password" name="confirm-pass" class="form-control" id="confrimPassword" required>
+                        </div>
+                        <small><?php echo $newPassError ?></small>
+                        <hr>
+  
+                        <button type="submit" class="btn btn-primary">Update</button>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        
+        
+        
         
         <?php elseif($_GET['tab'] == 'profile'): ?>
-            <div style="margin-bottom: 40px;" class="profile container col-lg-8 cil-md-6 col-sm-6 col-xs-6"  >
+            <div style="margin-bottom: 40px;" class="profile container col-lg-8 col-md-8 col-sm-12 col-xs-12"  >
                 <div class="profile_card card" style="width:70%;margin:0 auto; ">
                 <img profile-i style="margin:20px auto;width:50% ;box-shadow:0px 0px 10px black;" class="card-img-top img-profile rounded-circle" src="img/undraw_profile.svg">
                 <div class="card-body">
                     <h4 style="text-align: center; "><?php echo getName($data)?> </h4>
                     <hr class="divider">
-                    <table style="font-size: 20px;">
+                    <table >
                         <tr>
-                            <th style="width:40%">Email:</th>
-                            <td><?php echo getEmail($data) ?></td>
+                            <th style="padding-right:30px">Email:</th>
+                            <td style="font-size: larger;"><?php echo getEmail($data) ?></td>
                         </tr>
                         
                         <tr>
-                            <th>Contact:</th>
+                            <th style="padding-right:30px">Contact:</th>
                             <td><?php echo getContact($data) ?></td>
                         </tr>
                     </table>
