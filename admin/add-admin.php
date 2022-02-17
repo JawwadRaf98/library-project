5cb9cba058144e880d2f583c59fb908acaff5b03
JawwadRaf98 <55888087+JawwadRaf98@users.noreply.github.com>
2022-02-17 13:44:36 +0500

    dashboard added

--- admin/add-admin.php
+++ admin/add-admin.php
@@ -4,8 +4,13 @@
     include('includes/header.php');
     include('includes/sidebar.php');
     include('includes/config.php');
+    $success = false;
+    $successMsg = "";
+    $err= false;
+    $errMsg = '';
 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
+        
         $name = $_POST['name'];
         $lname = $_POST['last-name'];
         $email = $_POST['email'];
@@ -13,8 +18,30 @@
         $pass1 = $_POST['password1'];
         $contact = $_POST['contact'];
 
+        $query = "select * from admin where adminEmail = '{$email}' LIMIT 1";
+        $result = mysqli_query($connection, $query) or die("Query failed");
+        $data = mysqli_num_rows($result);
 
-        echo $name. ' '.$lname.' '.$email.' '.$pass.' '.$pass1;
+        if($data<1){
+            if($pass != $pass1){
+                $err = true;
+                $errMsg = "Password doesn't matched";
+            }
+            else{
+                $query = "insert into admin (adminName, adminLastName, adminEmail, adminPassword, adminContact) values ('{$name}','{$lname}','{$email}','{$pass}','{$contact}')";
+                $result = mysqli_query($connection,$query) or die("Query failed");
+                
+            }
+            
+        }
+        else{
+            $err = true;
+            $errMsg = "This email id already exist";
+        }
+
+
+
+        //echo $name. ' '.$lname.' '.$email.' '.$pass.' '.$pass1.' '. $contact.' ' .$data;
     }
 ?>
 
@@ -32,6 +59,14 @@
        <div class="container add-admin">
            <div class="row">
                <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 add-admin-col">
+                <?php
+                    if($err){
+                        echo '<div class="alert alert-primary" role="alert">';
+                        echo $errMsg;
+                        echo '</div>';
+                    }
+                ?>
+
                <form action = "" method="POST">
                     <div class="mb-3">
                         <label for="name" class="form-label">Name</label>
--- admin/index.php
+++ admin/index.php
@@ -93,23 +93,25 @@
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Admin
                                             </div>
                                             <div class="row no-gutters align-items-center">
                                                 <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="progress progress-sm mr-2">
-                                                        <div class="progress-bar bg-info" role="progressbar"
-                                                            style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                                            aria-valuemax="100"></div>
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                                                    <?php 
+                                                    $query = "SELECT * FROM admin";
+                                                        $run_query = mysqli_query($connection, $query);
+                                                        $data = mysqli_fetch_all($run_query);
+
+                                                        echo count($data);
+                                                    ?>
                                                     </div>
                                                 </div>
+                                                
                                             </div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                            <i class="fas fa-user fa-2x text-black-600"></i>
                                         </div>
                                     </div>
                                 </div>
@@ -118,16 +120,24 @@
 
                         <!-- Pending Requests Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-warning shadow h-100 py-2">
+                            <div class="card border-left-danger shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Pending Requests</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                                Categories</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                <?php 
+                                                $query = "SELECT * FROM category";
+                                                $run_query = mysqli_query($connection, $query);
+                                                $data = mysqli_fetch_all($run_query);
+
+                                                echo count($data);
+                                                ?>
+                                            </div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                            <i class="fas fa-fw fa-list-alt fa-2x text-black-600"></i>
                                         </div>
                                     </div>
                                 </div>
